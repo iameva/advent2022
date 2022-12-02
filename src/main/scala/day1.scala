e3d7613b8e36f0d6b51e767342054ea96b992315
David Lewis <duve.duve@gmail.com>
2022-12-01 22:10:40 -0700

    day2!

--- src/main/scala/day1.scala
+++ src/main/scala/day1.scala
@@ -1,4 +1,6 @@
 import scala.io.Source
+import java.util.PriorityQueue
+import scala.collection.JavaConverters._
 
 trait Puzzle {
   def main(args: Array[String]): Unit = {
@@ -31,8 +33,6 @@ object day1a extends Puzzle {
 }
 
 object day1b extends Puzzle {
-  import java.util.PriorityQueue
-  import scala.collection.JavaConverters._
   def solve(input: Iterator[String]): Any = {
     var current = 0
     val queue = new PriorityQueue[Int]
@@ -49,5 +49,63 @@ object day1b extends Puzzle {
       }
 
     queue.iterator().asScala.sum
+  }
+}
+
+object day2a extends Puzzle {
+  def solve(input: Iterator[String]): Any = {
+    var score = 0
+    input
+      .foreach { line =>
+        line.split(" ") match {
+          case Array(opponent, me) =>
+            score += (me match {
+              case "X" => 1
+              case "Y" => 2
+              case "Z" => 3
+              case _   => 0
+            })
+            score += ((me, opponent) match {
+              case ("X", "A") => 3
+              case ("X", "B") => 0
+              case ("X", "C") => 6
+              case ("Y", "A") => 6
+              case ("Y", "B") => 3
+              case ("Y", "C") => 0
+              case ("Z", "A") => 0
+              case ("Z", "B") => 6
+              case ("Z", "C") => 3
+              case _          => 0
+            })
+        }
+      }
+
+    score
+  }
+}
+
+object day2b extends Puzzle {
+  def solve(input: Iterator[String]): Any = {
+    var score = 0
+    input
+      .foreach { line =>
+        line.split(" ") match {
+          case Array(opponent, me) =>
+            score += ((me, opponent) match {
+              case ("X", "A") => 3
+              case ("X", "B") => 1
+              case ("X", "C") => 2
+              case ("Y", "A") => 1 + 3
+              case ("Y", "B") => 2 + 3
+              case ("Y", "C") => 3 + 3
+              case ("Z", "A") => 2 + 6
+              case ("Z", "B") => 3 + 6
+              case ("Z", "C") => 1 + 6
+              case _          => 0
+            })
+        }
+      }
+
+    score
   }
 }
